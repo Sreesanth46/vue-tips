0aa1fb8218ab5d1da035ef31afc0208a69daaf81
Sreesanth <sreesanth46@gmail.com>
2024-03-24 04:16:32 +0530

    chore: use formkit/auto-animate, vueuse/motion

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import './tailwind.css'
 import App from './App.vue'
 import router from "~/router/index.js";
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
+import { MotionPlugin } from '@vueuse/motion';
 
 const app = createApp(App)
 app.use(router)
+app.use(autoAnimatePlugin)
+app.use(MotionPlugin)
 app.mount('#app')
