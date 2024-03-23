cafd0465386c00539681dd08f5a11375e2437706
Sreesanth <sreesanth46@gmail.com>
2024-03-24 04:17:22 +0530

    feat: auto animate plugin

---
+++ src/pages/AutoAnimate.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+import { shuffleArray } from '~/helpers';
+
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const colors = ref(Array.from({ length: 20 }, () => randomColor()));
+</script>
+
+<template>
+  <button
+    class="w-full text-lg bg-slate-200 rounded-sm"
+    type="button"
+    @click="shuffleArray(colors)">
+    Shuffle
+  </button>
+  <div class="flex flex-wrap w-[500px] mx-auto" v-auto-animate>
+    <div
+      class="w-[100px] h-[100px] m-[5px] border border-black"
+      v-for="(color, i) in colors"
+      :key="color"
+      @click="colors.splice(i, 1)"
+      :style="{ backgroundColor: color }"></div>
+  </div>
+</template>
