78a10595549aa4f8a904c16e7c13c91afc3217d4
Sreesanth <sreesanth46@gmail.com>
2024-03-23 23:39:51 +0530

    feat: address page

---
+++ src/pages/AddressPage.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {reactive} from "vue";
+import AddressForm from "~/components/AddressForm.vue";
+
+const address = reactive({
+  firstName: "",
+  lastName: "",
+  country: "",
+  city: "",
+  postalCode: NaN,
+});
+</script>
+
+<template>
+  <div>
+    <AddressForm
+        v-model:first-name="address.firstName"
+        v-model:last-name="address.lastName"
+        v-model:country="address.country"
+        v-model:city="address.city"
+        v-model:postal-code="address.postalCode"
+    />
+
+    Name: {{ address.firstName + " " + address.lastName }}
+    <br/>
+    Country: {{ address.country }}
+    <br/>
+    City: {{ address.city }}
+    <br/>
+    Postal code: {{ address.postalCode }}
+  </div>
+</template>
