956ea8b9f1a12618ad9a3a7b38712649a10ee1e8
Sreesanth <sreesanth46@gmail.com>
2024-03-23 23:40:29 +0530

    feat: header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {routes} from "~/router/index.js";
+</script>
+
+<template>
+  <header class="py-4">
+    <nav>
+      <ul class="flex gap-2 mx-4">
+        <li>
+          <router-link
+              v-for="{name} in routes"
+              :to="{ name: name }"
+              :key="name"
+              class="text-blue-500 hover:text-blue-800 p-2"
+          >
+            {{ name }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
