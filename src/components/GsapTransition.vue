c6e61582a89e10e9c5564c51b27ea6a3ba91ab54
Sreesanth <sreesanth46@gmail.com>
2024-03-23 23:40:13 +0530

    feat: gsap transition component

---
+++ src/components/GsapTransition.vue
@@ -0,0 +1,43 @@
+<script setup>
+import gsap from "gsap";
+
+function beforeEnter(el) {
+  gsap.set(el, {
+    scaleX: 0.25,
+    scaleY: 0.52,
+    opacity: 1
+  })
+}
+
+function onEnter(el, done) {
+  gsap.to(el, {
+    duration: 1,
+    scaleX: 1,
+    scaleY: 1,
+    opacity: 1,
+    ease: "elastic.inOut(2.5, 1)",
+    onComplete: done
+  })
+}
+
+function onLeave(el, done) {
+  gsap.to(el, {
+    duration: 1,
+    scaleX: 0,
+    scaleY: 0,
+    opacity: 0.2,
+    ease: "elastic.inOut(2.5, 1)",
+    onComplete: done
+  })
+}
+</script>
+
+<template>
+  <Transition
+      @before-enter="beforeEnter"
+      @enter="onEnter"
+      @leave="onLeave"
+  >
+    <slot></slot>
+  </Transition>
+</template>
